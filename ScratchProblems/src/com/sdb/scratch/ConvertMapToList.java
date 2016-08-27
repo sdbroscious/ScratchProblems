b5bb64f1fcc02bad831c6b6892fdcc0fbbe47cec
david broscious <dbroscious@icloud.com>
2016-08-27 14:23:55 -0400

    minor update

--- ScratchProblems/src/com/sdb/scratch/ConvertMapToList.java
+++ ScratchProblems/src/com/sdb/scratch/ConvertMapToList.java
@@ -12,15 +12,21 @@ public class ConvertMapToList {
         map.put(30, "banana");
         map.put(40, "watermelon");
         map.put(50, "dragonfruit");
+        
         System.out.println("\n1. Export Map Key to List...");
+        
         List<Integer> result = map.entrySet().stream()
                 .map(x -> x.getKey())
                 .collect(Collectors.toList());
+        
         result.forEach(System.out::println);
+        
         System.out.println("\n2. Export Map Value to List...");
+        
         List<String> result2 = map.entrySet().stream()
                 .map(x -> x.getValue())
                 .collect(Collectors.toList());
+        
         result2.forEach(System.out::println);
     }
 
